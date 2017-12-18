2e73604f554ea41b4cb09594ec9eeeb27874741c
Brian Phillips <brian.phillips@qq.com>
2017-12-18 17:19:44 +0800

    init commit

---
+++ Console.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Console : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		float location = 3.0f; //Distance in meters
+		float homeLocation = 2.1f;
+		float distance = homeLocation - location;
+
+		print("Hello World!");
+		print("A game where you need to find your way back.");
+		print("Distance: " + distance);
+
+		if (location > homeLocation) {
+			print("Go back to get home");
+		}
+		if (location < homeLocation) {
+			print("Go forward to get home");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
